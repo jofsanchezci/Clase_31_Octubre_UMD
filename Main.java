0019a1ba5aaae93613a9b02c620fea76a6d28df1
js012108 <js012108@gmail.com>
2023-10-31 20:18:22 -0500

    Clase 31 de Octubre

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	// Esto es un comentario
+  public static void main(String[] args) {	
+    System.out.println("Hola Mundo");
+    System.out.println("Hoy es 31 de Octubre");
+    System.out.println("Hoy es Miercoles");
+    System.out.println(3 + 3);
+  }
+}
+
+
---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+	// Esto es un comentario
+  public static void main(String[] args) {	
+    System.out.println("Hola Mundo");
+    System.out.println("Hoy es 31 de Octubre");
+    System.out.println("Hoy es Miercoles");
+    System.out.println(3 + 3);
+  }
+}
